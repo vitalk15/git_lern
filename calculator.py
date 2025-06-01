8051d85512c5ace81d25f300d509e8c0af22cd9b
Vital <vital.15@mail.ru>
2025-06-01 22:58:34 +0300

    Шаблон калькулятора

---
+++ calculator.py
@@ -0,0 +1,9 @@
+class Calculator:
+    last_res = None
+
+    def sum(self, n1, n2):
+        self.last_res = n1 + n2
+        return n1 + n2
+
+    def print_lr(self):
+        print(self.last_res)
---
+++ test_calc.py
@@ -0,0 +1,6 @@
+from calculator import Calculator
+
+
+calc = Calculator()
+calc.sum(10, 5)
+calc.print_lr()
